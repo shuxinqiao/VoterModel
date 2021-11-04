e138eb06aea787af9e49811ba2b0151026800c07
shuxinqiao <sqiao@ualberta.ca>
2021-11-04 16:08:57 -0600

    plot color red fiexd

--- votermodel/test/test_main.py
+++ votermodel/test/test_main.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    n = 3000
+    n = 50
     pop_rate = []
-    run_times = [0,]
+    run_times = []
 
     #node_vector_num = create_node_name_vector(n, "number")
     #print(node_vector_num)
@@ -21,15 +21,16 @@ def main():
 
     node_value_vector = create_node_value_vector(n)
     print(node_value_vector)
-    print('AYaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
     node_binary_matrix = create_binary_matrix(n)
-    print(node_binary_matrix)
+    print(node_binary_matrix)   
+    #print(node_binary_matrix[1])
 
     pop_rate.append(np.mean(node_value_vector))
-    print('whuooooooooooooooooo')
+    run_times.append(0)
 
     for i in range(500):
-        print("-----run ",i," -----------------------")
+        print("----------run ",i," ------------------")
         transition_rate_vector = transition_rate(node_binary_matrix, node_value_vector)
         #print(transition_rate_vector)
 
@@ -42,24 +43,17 @@ def main():
         node_value_vector = change_node_value(node_value_vector,transition_rate_vector,reverse_transition_rate_vector)
         print(node_value_vector)
 
+
         pop_rate.append(np.mean(node_value_vector))
         run_times.append(i+1)
         
 
-        #plt.plot(i, pop_rate[i])
-        plt.plot(run_times,pop_rate)
-        #plt.pause(0.05)
+        plt.plot(run_times,pop_rate,"red")
 
-        #plt.gca().lines[0].set_xdata(run_times[i])
-        #plt.gca().lines[0].set_ydata(pop_rate[i])
-        #plt.gca().relim()
-        #plt.gca().autoscale_view()
         plt.pause(0.05)
 
     plt.show()
 
-
-    #new_node_value_vector = change_node_value()
 
     return 0
 
